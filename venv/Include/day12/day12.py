4a962ff5f455e3c5621db6b417de89d6b5d7cba2
Jack <280147597@qq.com>
2020-04-15 21:20:07 +0800

    字符串与正则表达式Exercise

---
+++ venv/Include/day12/day12.py
@@ -0,0 +1,63 @@
+"""
+使用正则表达式
+author: Jack Lee
+time: 2020/4/14 10:26
+
+"""
+
+"""
+验证输入用户名和QQ号是否有效并给出对应的提示信息
+
+要求：用户名必须由字母、数字或下划线构成且长度在6~20个字符之间，QQ号是5~12的数字且首位不能为0
+"""
+
+# Python提供re模块来支持正则表达式相关操作
+import re
+
+
+# 例子1：验证输入用户名和QQ号是否有效并给出对应的提示信息
+
+def check_username_qq():
+    username = input("Username: ")
+    qq = input("Input the qq number: ")
+
+    # re.match 用正则表达式匹配字符串 成功返回匹配对象，失败返回None
+    # 字符串前加r表示使用原始字符串，即字符串中没有所谓的转义字符。
+    m1 = re.match(r'^[0-9a-zA-Z_]{6,20}$', username)
+    if not m1:
+        print('Please input the valid username')
+    else:
+        print("This username is valid")
+    m2 = re.match(r'^[1-9]\d{4,11}$', qq)
+    if not m2:
+        print('Please input the valid qq')
+    else:
+        print("This qq number is valid")
+
+
+# 例子2：从一段文字中提取出国内手机号码。
+
+def find_all_number():
+    # 编译正则表达式返回正则表达式对象
+    # 实际开发中若如果一个正则表达式需要重复的使用, 那么先通过compile函数编译正则表达式并创建出正则表达式对象无疑是更为明智的选择。
+    pattern = re.compile(r'(?<=\D)1[34578]\d{9}(?=\D)')
+    sentence = '''
+        重要的事情说8130123456789遍，我的手机号是13512346789这个靓号，
+    不是15600998765，也是110或119，王大锤的手机号才是15600998765。
+    '''
+
+    # re.findall查找字符串所有与正则表达式匹配的模式 返回字符串列表
+    mylist = re.findall(pattern, sentence)
+    print(mylist)
+    print("---------------------------------------")
+
+    # re.finditer 查找字符串与正则表达式匹配的模式，返回一个迭代器
+    # 通过迭代器取出匹配对象并获得匹配内容
+    # （实际开发中也可以用正则表达式对象的方法pattern.finditer替代re.finditer
+    for temp in re.finditer(pattern, sentence):
+        # temp是一个匹配对象， 通过匹配对象.group()来得到整个该匹配对象中匹配的字符串
+        print(temp.group())
+
+if __name__ == '__main__':
+    check_username_qq()
+    find_all_number()
