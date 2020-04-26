d38c16440a18a12d394cd2313fd552d7a26602d8
Jack <280147597@qq.com>
2020-04-26 20:58:13 +0800

    正则表达式:更新practice 3

--- venv/Include/day12/day12.py
+++ venv/Include/day12/day12.py
@@ -69,8 +69,18 @@ def replace_main():
     purified = re.sub("[操肏艹]|fuck|shit|傻[比屄逼叉缺吊屌]|煞笔|fuck you", '*', sentence, flags=re.I)
     print(purified)
 
+# 例子4：拆分长字符串
+
+def split_main():
+    poem = '窗前明月光，疑是地上霜。举头望明月，低头思故乡。'
+    # 用正则表达式指定的模式分隔符拆分字符串 返回列表
+    sentence_list = re.split(r"[,。,.]", poem)
+    while '' in sentence_list:
+        sentence_list.remove('')
+    print(sentence_list)
 
 if __name__ == '__main__':
     check_username_qq()
     find_all_number()
     replace_main()
+    split_main()
