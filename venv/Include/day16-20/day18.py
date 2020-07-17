215ad947c7c197be18ad5cef6788db137de5f144
Jack <280147597@qq.com>
2020-07-17 17:54:37 +0800

    day18： 元类实现单例模式

--- venv/Include/day16-20/day18.py
+++ venv/Include/day16-20/day18.py
@@ -156,6 +156,7 @@ try:
     my_dict['username'] = 'helloJack'
 
 except KeyError:
+    print(KeyError)
     pass
 
 print(my_dict)
@@ -164,10 +165,51 @@ dict1 = dict()
 dict1['name'] = 'Jack'
 print(dict1)
 
+"""元编程和元类"""
+# 元编程和元类:对象通过类创建，类通过元类创建，元类提供了创建类的元信息。所有的类都直接或者间接的继承自object，object是由type创建的
+# 所有的元类都直接或间接地继承自type，type本身就是一个元类
 
+# 使用元类实现单例模式 这里是线程安全的单例模式
 
+import threading
 
+# 自定义元类
+class SingletonMeta(type):
 
+    # cls代表这个类本身 self代表一个类的实例本身
+    def __init__(cls, *args, **kwargs):
+        cls.__instance = None
+        cls.__lock = threading.RLock()
+        super().__init__(*args, **kwargs)
 
+    def __call__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            # 创建实例前先加锁 类似装饰器类构建单例模式部分 day17
+            with cls.__lock:
+                if cls.__instance is None:
+                    cls.__instance = super().__call__(*args, **kwargs)
+        return cls.__instance
+
+class President(metaclass=SingletonMeta):
+
+    def __init__(self, name):
+        self._name = name
+
+    def say(self):
+        print("I am the president: " + self._name)
+
+# 一个类的类型是元类
+print(type(President))
+
+president1 = President("Jack")
+# 一个实例的类型是类
+print(type(president1))
+# 一个元类的类型是type， type是Python在背后创建所有类的元类
+print(type(SingletonMeta))
+
+president1.say()
+# 只会创建Jack总统1， 总统2不会创建
+president2 = President("Tom")
+president2.say()
 
 
